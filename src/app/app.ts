6c65814bdbb0d2257cb81e9b3194695f2567fa5c
Jose Cuzmar <josecuzmar@josecuzmar.local>
2026-09-21 15:58:00 -0300

    Use the signal function to create a signal for holding local state

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+// Create a signal with the `signal` function.
+const firstName = signal('Morgan');
+
+// Read a signal value by calling it— signals are functions.
+console.log(firstName());
+
+// Change the value of this signal by calling its `set` method with a new value.
+firstName.set('Jaime');
+console.log(firstName());
+
+// You can also use the `update` method to change the value
+// based on the previous value.
+firstName.update((name) => name.toUpperCase());
+console.log(firstName());
 
 @Component({
-  imports: [RouterOutlet],
   selector: 'app-root',
-  styleUrl: './app.css',
-  templateUrl: './app.html',
+  template: `
+    <p>See the browser\'s console to check info related to Signals feature, in this excersise. </p>
+    <p>This example is based on Angular Essentials Signals documentation:</p>
+    <a href="https://angular.dev/essentials/signals" target="_blank">Essentials/Signals</a>
+  `,
 })
 export class App {
-  protected readonly title = signal('ng-essentials');
 }
