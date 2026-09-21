6b487c259e07923f434ad770c976730ee30ff964
Jose Cuzmar <josecuzmar@josecuzmar.local>
2026-09-21 16:07:42 -0300

    A computed is a signal that produces its value based on other signals.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,26 +1,14 @@
-import { Component, signal } from '@angular/core';
+import {Component, signal, computed} from '@angular/core';
 
-// Create a signal with the `signal` function.
 const firstName = signal('Morgan');
+const firstNameCapitalized = computed(() => firstName().toUpperCase());
 
-// Read a signal value by calling it— signals are functions.
-console.log(firstName());
-
-// Change the value of this signal by calling its `set` method with a new value.
-firstName.set('Jaime');
-console.log(firstName());
-
-// You can also use the `update` method to change the value
-// based on the previous value.
-firstName.update((name) => name.toUpperCase());
-console.log(firstName());
+console.log(firstNameCapitalized()); // MORGAN
 
 @Component({
   selector: 'app-root',
   template: `
-    <p>See the browser\'s console to check info related to Signals feature, in this excersise. </p>
-    <p>This example is based on Angular Essentials Signals documentation:</p>
-    <a href="https://angular.dev/essentials/signals" target="_blank">Essentials/Signals</a>
+    <p>Check the console in the browser</p>
   `,
 })
 export class App {
