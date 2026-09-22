c24518523945b0e956aedb0e6d64f4931d029ad5
Jose Cuzmar <josecuzmar@josecuzmar.local>
2026-09-22 11:45:46 -0300

    add other input types in the form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,16 @@
 import {Component, signal} from '@angular/core';
 import {form, FormField} from '@angular/forms/signals';
 
-interface LoginData {
+// Adding some other data type to handle differents input types could be present in html form
+// see https://angular.dev/essentials/signal-forms for other input types.
+
+interface FormData {
   email: string;
-  password: string;
+  name: string;
+  age: number;
+  eventDate: Date,
+  eventTime: string,
+
 }
 
 @Component({
@@ -13,20 +20,27 @@ interface LoginData {
   imports: [FormField],
 })
 export class App {
-  loginModel = signal<LoginData>({
+  formModel = signal<FormData>({
     email: '',
-    password: '',
+    name: '',
+    age: 0,
+    eventDate: new Date(),
+    eventTime: '',
   });
 
-  loginForm = form(this.loginModel);
+  loginForm = form(this.formModel);
 
   onSubmit(event: Event) {
     event.preventDefault();
 
-    // Perform login logic here
-    const credentials = this.loginModel();
-    console.log('Logging in with:', credentials);
+    // Perform some logic here, could be validate, modify data, etc.
 
-    // e.g., await this.authService.login(credentials);
+    // harcoded the age
+    this.loginForm.age().value.set(18);
+
+
+    const formData = this.formModel();
+    console.log('Logging in with:', formData);
+
   }
 }
